671951ada2bb358ba5b487fc0406da15ccf32004
Ion Petrascu <ionpetrascuc0318@gmal.com>
2024-02-07 13:44:52 +0200

    modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
+const Modal = ({ openModal, setOpenModal, children }) => (
+  <div onClick={() => setOpenModal(false)} className={`overlay animated ${openModal && 'show'}`}>
+    <div className="modal">
+      <svg onClick={() => setOpenModal(false)} height="200" viewBox="0 0 200 200" width="200">
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      <img src={children} />
+    </div>
+  </div>
+);
+
 function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div> */}
+      <button onClick={() => setOpenModal(true)} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+
+      <Modal
+        setOpenModal={setOpenModal}
+        openModal={openModal}
+        children={'https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif'}
+      />
     </div>
   );
 }
